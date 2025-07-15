1747c86ad618e288ef70dfe5d0f7f5f1069c354d
theo-al <93951769+theo-al@users.noreply.github.com>
2025-07-15 11:03:47 -0300

    initialization_sequence.ino: formatação e claridade

--- examples/initialization_sequence/initialization_sequence.ino
+++ examples/initialization_sequence/initialization_sequence.ino
@@ -1,59 +1,33 @@
 #include "SumoIR.h"
 
 SumoIR IR;
-
-int strategy = 0; // estrategia
-
-void setup(){
+void setup() {
+    Serial.begin(115200);
   
-  Serial.begin(115200);
-  IR.begin(15); // sensor conectado no pino 15
-
-  // comente se não houver LED
-  IR.setLed(2,HIGH,180); // led no pino 2, acende quando esta HIGH e irá piscar com intervalo de 180ms
-
-  // IR.debug(false); // <- descomente se não quiser debug na serial
-
+    IR.begin(15); // sensor conectado no pino 15
+    IR.setLed(2,HIGH,180); // led no pino 2, acende quando esta HIGH e irá piscar com intervalo de 180ms
+    //^ comente se não houver LED
+  
+    // IR.debug(false); // <- descomente se não quiser debug na serial
 }
 
 void loop() {
-
     IR.update();
 
-    if ( IR.prepare() ) {
-
-      /* robô em preparação */
-
+    if (IR.prepare()) { /* robô em preparação */
       Serial.println("-> sumo prepare");
       delay(400);
-
-    }else if ( IR.start() ) {
-
-      /* chamado apenas uma vez quando se torna esse estado */
-      /* movimento inicial */
-      
+    } else if (IR.start()) { /* movimento inicial*/
+      // executa apenas na primeira transição para esse estado
       Serial.println("-> sumo start");
-    
-    } else if ( IR.on() ){
-
-      /* codigo do robô ligado */
-      
+    } else if (IR.on()) { /* código do robô ligado */
       Serial.println("-> sumo on");
       delay(400);
-      
-    }else if( IR.stop() ){
-      
-      /* chamado apenas uma vez quando se torna esse estado */
-      /* codigo que desliga o robô */
-
+    } else if (IR.stop()) { /* código que desliga o robô */
+      // executa apenas na primeira transição para esse estado
       Serial.println("-> sumo stop");
-
-    }else{
-
-      /* codigo do robô desligado */
-
+    } else { /* código do robô desligado */
       Serial.println("-> sumo off");
       delay(400);
-
     }
 }
